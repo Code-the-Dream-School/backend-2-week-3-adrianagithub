2e101528c701c9324ff78361144ed345f295239d
Adi <adi@Adis-MacBook-Air.local>
2020-10-04 10:51:15 -0700

    update sunday

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,7 @@ class Customer < ApplicationRecord
   validates :phone,  presence: true
   validates :phone, numericality: { only_integer: true }
   validates :phone, length: { is: 10}
+  #validates_length_of :number, is: 10,  message: "Number must be 10 digit long" 
   validates :email, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
        
@@ -11,3 +12,13 @@ class Customer < ApplicationRecord
     "#{first_name} #{last_name}"
   end       
 end
+
+#only integer
+#/\A[+-]?\d+\z/
+
+# validates_format_of :phone, :with => /\d[0-9]\)*\z/ , 
+# :message => "Only positive number without spaces are allowed" 
+
+# validates :phone, :presence => {:message => 'hello world, bad operation!'}, 
+#     :numericality => true, 
+#     :length => { :minimum => 10, :maximum => 15 } 
