6ddb6c10f4f96c7b084991d74068998cd37515b0
Adriana Cabrera <adi_111@icloud.com>
2020-10-11 21:34:12 -0700

    Last update with rspec

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,6 +2,10 @@ FactoryBot.define do
   factory :order do
     product_name { "MyString" }
     product_count { 1 }
-    customer { nil }
+    #customer { nil }
+    customer_id {FactoryBot.create(:customer).id}
+    
   end
 end
+
+
